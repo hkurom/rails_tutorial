bb6b37eacde7b938429ef650f7af5a21868d9664
hkurom <hkurom@gmail.com>
2018-07-28 06:00:39 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,7 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
     @other_user = users(:archer)
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -67,5 +67,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
